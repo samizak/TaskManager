4bd7c21c777209f6d14434b029a827797da80a76
sami zakir <samizak1864@gmail.com>
2021-04-21 19:45:41 +0100

    Version 1.2 Final Version for Sprint 1

--- app/src/main/java/com/example/finalyearproject/AddNewTaskBottomSheet.java
+++ app/src/main/java/com/example/finalyearproject/AddNewTaskBottomSheet.java
@@ -2,6 +2,7 @@ package com.example.finalyearproject;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +19,16 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class AddNewTaskBottomSheet extends BottomSheetDialogFragment {
 
+    private final ToDoModel updateTaskModel = new ToDoModel();
+
     private EditText enterTaskEditText;
     private Button saveButton;
 
     private Context context;
+
+    private boolean isUpdateTask = false;
+
+    private String tempTaskName;
 
     private void SaveButtonListener(View v) {
         String taskName = enterTaskEditText.getText().toString().trim();
@@ -37,12 +44,64 @@ public class AddNewTaskBottomSheet extends BottomSheetDialogFragment {
         taskModel.setTaskName(taskName);
         taskModel.setIsCompleted(false);
 
-        SaveTask(taskModel);
+        CreateNewTask(taskName);
+        UpdateTask(taskName);
 
         dismiss();
     }
-    private void SaveTask(ToDoModel taskModel) {
-        TaskActivity.taskList.add(taskModel);
+    private void SaveTask(ToDoModel taskModel, boolean isPushNewTask) {
+        if (isPushNewTask) {
+            TaskActivity.taskList.add(taskModel);
+            return;
+        }
+
+        for(ToDoModel toDoModel : TaskActivity.taskList){
+            if(taskModel.getTaskName().equals(tempTaskName)){
+                toDoModel.setTaskName(taskModel.getTaskName());
+                toDoModel.setIsCompleted(taskModel.getIsCompleted());
+            }
+        }
+    }
+
+    private void CreateNewTask(String taskName) {
+        // Skip if we are not Creating a new Task...
+        if (isUpdateTask) return;
+
+        ToDoModel taskModel = new ToDoModel();
+
+        taskModel.setTaskName(taskName);
+        taskModel.setIsCompleted(false);
+
+        SaveTask(taskModel, true);
+    }
+
+    private void UpdateTask(String taskName) {
+        // Skip if we not Updating an existing Task
+        if (!isUpdateTask) return;
+
+        ToDoModel taskModel = new ToDoModel();
+        tempTaskName = taskName;
+
+        taskModel.setId(updateTaskModel.getId());
+        taskModel.setTaskName(taskName);
+        taskModel.setIsCompleted(updateTaskModel.getIsCompleted());
+
+        SaveTask(taskModel, false);
+    }
+
+
+    // Automatically fill data if editing a Task
+    private void AutoFillIfEditingTask(Bundle bundle) {
+        // Only automatically fill details when in edit mode!
+        if (bundle == null) return;
+
+        isUpdateTask = true;
+
+        updateTaskModel.setId(bundle.getString("taskID"));
+        updateTaskModel.setTaskName(bundle.getString("taskName"));
+        updateTaskModel.setIsCompleted(Boolean.parseBoolean(bundle.getString("taskCompleted")));
+
+        enterTaskEditText.setText(updateTaskModel.getTaskName());
     }
 
     @Override
@@ -51,6 +110,9 @@ public class AddNewTaskBottomSheet extends BottomSheetDialogFragment {
 
         saveButton = v.findViewById(R.id.saveTaskButton);
         enterTaskEditText = v.findViewById(R.id.enterTaskText);
+
+        AutoFillIfEditingTask(getArguments());
+
 
         // Handle Saving the Task data
         saveButton.setOnClickListener(this::SaveButtonListener);
--- app/src/main/java/com/example/finalyearproject/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/finalyearproject/RecyclerViewAdapter.java
@@ -1,14 +1,19 @@
 package com.example.finalyearproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.finalyearproject.RecyclerViewAdapter.MyViewHolder;
@@ -26,6 +31,51 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<MyViewHolder> {
         this.taskList = taskList;
     }
 
+    private void MoreOptionsPopupListener(@NonNull MyViewHolder holder, View v, int position) {
+        PopupMenu popup = new PopupMenu(v.getContext(), holder.moreOptionsImageView);
+        popup.inflate(R.menu.task_row_more_options_popup);
+        popup.show();
+
+        popup.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.taskEdit) EditTaskPopupListener(v, position);
+            else if (item.getItemId() == R.id.deleteTask) DeleteTaskPopupListener(v, position);
+            return true;
+        });
+    }
+
+    private void EditTaskPopupListener(View v, int position) {
+        ToDoModel taskModel = taskList.get(position);
+        Bundle bundle = new Bundle();
+        bundle.putString("taskID", taskModel.getId());
+        bundle.putString("taskName", taskModel.getTaskName());
+        bundle.putString("isCompleted", String.valueOf(taskModel.getIsCompleted()));
+
+        AddNewTaskBottomSheet addNewTaskBottomSheet = new AddNewTaskBottomSheet();
+        addNewTaskBottomSheet.setArguments(bundle);
+        addNewTaskBottomSheet.show(((AppCompatActivity) v.getContext()).getSupportFragmentManager(), "addNewTaskBottomSheet");
+    }
+
+    private void DeleteTaskPopupListener(View v, int position) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(v.getContext());
+        dialogBuilder.setTitle("Delete?");
+        dialogBuilder.setMessage("Are you sure you want to delete the selected Tasks?");
+
+        // Yes Button
+        dialogBuilder.setPositiveButton("Yes", (dialog, id_) -> {
+            ToDoModel taskModel = taskList.get(position);
+            taskList.remove(taskModel);
+            this.notifyDataSetChanged();
+
+            taskList.remove(taskModel);
+        });
+
+        // No Button
+        dialogBuilder.setNegativeButton("No", (dialog, id_) -> dialog.dismiss());
+
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -41,6 +91,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<MyViewHolder> {
         // Set the Task Name
         holder.taskNameTextView.setText(taskName);
 
+        // More options button Clicked
+        holder.moreOptionsImageView.setOnClickListener(v -> MoreOptionsPopupListener(holder, v, position));
+
         // Checkbox listener
         holder.taskCompletedCheckBox.setOnCheckedChangeListener((checkbox, isChecked) ->{
             Toast.makeText(activity, "Task completed: " + isChecked, Toast.LENGTH_LONG).show();
@@ -54,12 +107,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         private final TextView taskNameTextView;
+
+        private final ImageView moreOptionsImageView;
+
         private final CheckBox taskCompletedCheckBox;
 
         public MyViewHolder(@NonNull View view) {
             super(view);
 
             taskNameTextView = view.findViewById(R.id.taskName);
+
+            moreOptionsImageView = view.findViewById(R.id.moreOptionsImageView);
+
             taskCompletedCheckBox = view.findViewById(R.id.taskCompleted);
         }
     }
